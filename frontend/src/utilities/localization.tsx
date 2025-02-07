80e5cbf1082916d4f186528c7b5936c3322bcb78
Nils Kreiner <kreiner@welnick02.local>
2025-02-07 13:10:35 +0100

    finished adding tooltips for the CVSS calculator metrics

--- frontend/src/utilities/localization.tsx
+++ frontend/src/utilities/localization.tsx
@@ -6,6 +6,7 @@ const LOW = "Low";
 const MEDIUM = "Medium";
 const HIGH = "High";
 const NONE = "None";
+const NOT_DEFINED_DEF = "The default value.";
 const language = {
     de: {
         breadCrumbMap: {
@@ -605,50 +606,50 @@ const language = {
                             high: {title: HIGH, tooltip: "Loss of [Confidentiality | Integrity | Availability] is likely to have a catastrophic adverse effect on the organization or individuals associated with the organization (e.g., employees, customers)."}
                         },
                         modifiedAttackVector: {
-                            notDefined: {title: NOT_DEFINED, tooltip: "The default value."},
+                            notDefined: {title: NOT_DEFINED, tooltip: NOT_DEFINED_DEF},
                             network: {title: "Network", tooltip: "The vulnerable component is bound to the network stack and the set of possible attackers extends beyond the other options listed below, up to and including the entire Internet. Such a vulnerability is often termed “remotely exploitable” and can be thought of as an attack being exploitable at the protocol level one or more network hops away (e.g., across one or more routers). An example of a network attack is an attacker causing a denial of service (DoS) by sending a specially crafted TCP packet across a wide area network (e.g., CVE‑2004‑0230)."},
                             adjacent: {title: "Adjacent", tooltip: "The vulnerable component is bound to the network stack, but the attack is limited at the protocol level to a logically adjacent topology. This can mean an attack must be launched from the same shared physical (e.g., Bluetooth or IEEE 802.11) or logical (e.g., local IP subnet) network, or from within a secure or otherwise limited administrative domain (e.g., MPLS, secure VPN to an administrative network zone). One example of an Adjacent attack would be an ARP (IPv4) or neighbor discovery (IPv6) flood leading to a denial of service on the local LAN segment (e.g., CVE‑2013‑6014)."},
                             local: {title: "Local", tooltip: "The vulnerable component is not bound to the network stack and the attacker’s path is via read/write/execute capabilities. Either: the attacker exploits the vulnerability by accessing the target system locally (e.g., keyboard, console), or remotely (e.g., SSH); or the attacker relies on User Interaction by another person to perform actions required to exploit the vulnerability (e.g., using social engineering techniques to trick a legitimate user into opening a malicious document)."},
                             physical: {title: "Physical", tooltip: "The attack requires the attacker to physically touch or manipulate the vulnerable component. Physical interaction may be brief (e.g., evil maid attack1) or persistent. An example of such an attack is a cold boot attack in which an attacker gains access to disk encryption keys after physically accessing the target system. Other examples include peripheral attacks via FireWire/USB Direct Memory Access (DMA)."}
                         },
                         modifiedAttackComplexity: {
-                            notDefined: {title: NOT_DEFINED, tooltip: "somePlaceholder"},
-                            low: {title: LOW, tooltip: "somePlaceholder"},
-                            high: {title: HIGH, tooltip: "somePlaceholder"}
+                            notDefined: {title: NOT_DEFINED, tooltip: NOT_DEFINED_DEF},
+                            low: {title: LOW, tooltip: "Specialized access conditions or extenuating circumstances do not exist. An attacker can expect repeatable success when attacking the vulnerable component."},
+                            high: {title: HIGH, tooltip: "A successful attack depends on conditions beyond the attacker's control. That is, a successful attack cannot be accomplished at will, but requires the attacker to invest in some measurable amount of effort in preparation or execution against the vulnerable component before a successful attack can be expected.2 For example, a successful attack may depend on an attacker overcoming any of the following conditions: The attacker must gather knowledge about the environment in which the vulnerable target/component exists. For example, a requirement to collect details on target configuration settings, sequence numbers, or shared secrets. or The attacker must prepare the target environment to improve exploit reliability. For example, repeated exploitation to win a race condition, or overcoming advanced exploit mitigation techniques. or The attacker must inject themselves into the logical network path between the target and the resource requested by the victim in order to read and/or modify network communications (e.g., a man in the middle attack)."}
                         },
                         modifiedPrivilegesRequired: {
-                            notDefined: {title: NOT_DEFINED, tooltip: "somePlaceholder"},
-                            none: {title: NONE, tooltip: "somePlaceholder"},
-                            low: {title: LOW, tooltip: "somePlaceholder"},
-                            high: {title: HIGH, tooltip: "somePlaceholder"}
+                            notDefined: {title: NOT_DEFINED, tooltip: NOT_DEFINED_DEF},
+                            none: {title: NONE, tooltip: "The attacker is unauthorized prior to attack, and therefore does not require any access to settings or files of the vulnerable system to carry out an attack."},
+                            low: {title: LOW, tooltip: "The attacker requires privileges that provide basic user capabilities that could normally affect only settings and files owned by a user. Alternatively, an attacker with Low privileges has the ability to access only non-sensitive resources."},
+                            high: {title: HIGH, tooltip: "The attacker requires privileges that provide significant (e.g., administrative) control over the vulnerable component allowing access to component-wide settings and files."}
                         },
                         modifiedUserInteraction: {
-                            notDefined: {title: NOT_DEFINED, tooltip: "somePlaceholder"},
-                            none: {title: NONE, tooltip: "somePlaceholder"},
-                            required: {title: "Required", tooltip: "somePlaceholder"}
+                            notDefined: {title: NOT_DEFINED, tooltip: NOT_DEFINED_DEF},
+                            none: {title: NONE, tooltip: "The vulnerable system can be exploited without interaction from any user."},
+                            required: {title: "Required", tooltip: "Successful exploitation of this vulnerability requires a user to take some action before the vulnerability can be exploited. For example, a successful exploit may only be possible during the installation of an application by a system administrator."}
                         },
                         modifiedScope: {
-                            notDefined: {title: NOT_DEFINED, tooltip: "somePlaceholder"},
-                            unchanged: {title: "Unchanged", tooltip: "somePlaceholder"},
-                            changed: {title: "Changed", tooltip: "somePlaceholder"}
+                            notDefined: {title: NOT_DEFINED, tooltip: NOT_DEFINED_DEF},
+                            unchanged: {title: "Unchanged", tooltip: "An exploited vulnerability can only affect resources managed by the same security authority. In this case, the vulnerable component and the impacted component are either the same, or both are managed by the same security authority."},
+                            changed: {title: "Changed", tooltip: "An exploited vulnerability can affect resources beyond the security scope managed by the security authority of the vulnerable component. In this case, the vulnerable component and the impacted component are different and managed by different security authorities."}
                         },
                         modifiedConfidentiality: {
-                            notDefined: {title: NOT_DEFINED, tooltip: "somePlaceholder"},
-                            none: {title: NONE, tooltip: "somePlaceholder"},
-                            low: {title: LOW, tooltip: "somePlaceholder"},
-                            high: {title: HIGH, tooltip: "somePlaceholder"}
+                            notDefined: {title: NOT_DEFINED, tooltip: NOT_DEFINED_DEF},
+                            none: {title: NONE, tooltip: "There is no loss of confidentiality within the impacted component."},
+                            low: {title: LOW, tooltip: "There is some loss of confidentiality. Access to some restricted information is obtained, but the attacker does not have control over what information is obtained, or the amount or kind of loss is limited. The information disclosure does not cause a direct, serious loss to the impacted component."},
+                            high: {title: HIGH, tooltip: "There is a total loss of confidentiality, resulting in all resources within the impacted component being divulged to the attacker. Alternatively, access to only some restricted information is obtained, but the disclosed information presents a direct, serious impact. For example, an attacker steals the administrator's password, or private encryption keys of a web server."}
                         },
                         modifiedIntegrity: {
-                            notDefined: {title: NOT_DEFINED, tooltip: "somePlaceholder"},
-                            none: {title: NONE, tooltip: "somePlaceholder"},
-                            low: {title: LOW, tooltip: "somePlaceholder"},
-                            high: {title: HIGH, tooltip: "somePlaceholder"}
+                            notDefined: {title: NOT_DEFINED, tooltip: NOT_DEFINED_DEF},
+                            none: {title: NONE, tooltip: "There is no loss of integrity within the impacted component."},
+                            low: {title: LOW, tooltip: "Modification of data is possible, but the attacker does not have control over the consequence of a modification, or the amount of modification is limited. The data modification does not have a direct, serious impact on the impacted component."},
+                            high: {title: HIGH, tooltip: "There is a total loss of integrity, or a complete loss of protection. For example, the attacker is able to modify any/all files protected by the impacted component. Alternatively, only some files can be modified, but malicious modification would present a direct, serious consequence to the impacted component."}
                         },
                         modifiedAvailability: {
-                            notDefined: {title: NOT_DEFINED, tooltip: "somePlaceholder"},
-                            none: {title: NONE, tooltip: "somePlaceholder"},
-                            low: {title: LOW, tooltip: "somePlaceholder"},
-                            high: {title: HIGH, tooltip: "somePlaceholder"}
+                            notDefined: {title: NOT_DEFINED, tooltip: NOT_DEFINED_DEF},
+                            none: {title: NONE, tooltip: "There is no impact to availability within the impacted component."},
+                            low: {title: LOW, tooltip: "Performance is reduced or there are interruptions in resource availability. Even if repeated exploitation of the vulnerability is possible, the attacker does not have the ability to completely deny service to legitimate users. The resources in the impacted component are either partially available all of the time, or fully available only some of the time, but overall there is no direct, serious consequence to the impacted component."},
+                            high: {title: HIGH, tooltip: "There is a total loss of availability, resulting in the attacker being able to fully deny access to resources in the impacted component; this loss is either sustained (while the attacker continues to deliver the attack) or persistent (the condition persists even after the attack has completed). Alternatively, the attacker has the ability to deny some availability, but the loss of availability presents a direct, serious consequence to the impacted component (e.g., the attacker cannot disrupt existing connections, but can prevent new connections; the attacker can repeatedly exploit a vulnerability that, in each instance of a successful attack, leaks a only small amount of memory, but after repeated exploitation causes a service to become completely unavailable)."}
                         } 
                     },
                 },
